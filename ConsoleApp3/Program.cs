887263d87144742b6c6d765b9e5b6483eeb2d32d
Valentin <valentin22812@gmail.com>
2024-02-26 03:58:37 +0800

    first

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Xml.Linq;
+
+namespace FirstApplication
+{
+    public class Logic
+    {
+        static public String answer(String name1, String name2)
+        {
+            var set = new Dictionary<char, int>();
+            String answer = "";
+            for (int i = 0; i < name1.Length; i++)
+            {
+                set[name1[i]] = 0;
+            }
+
+            for (int i = 0; i < name1.Length; i++)
+            {
+                set[name1[i]]++;
+
+                if (set[name1[i]] == 1 && name2.Contains(name1[i]))
+                {
+                    answer += "да ";
+                }
+                else if (set[name1[i]] == 1)
+                {
+                    answer += "нет ";
+                }
+            }
+
+
+            return answer;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var name1 = Console.ReadLine();
+            var name2 = Console.ReadLine();
+
+            var outMessage = Logic.answer(name1, name2);
+
+            Console.WriteLine(outMessage);
+
+
+
+        }
+
+    }
+}
+
---
+++ ConsoleApp3Tests/LogicTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FirstApplication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApplication.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void answerTest1()
+        {
+            var message = Logic.answer("процессор", "информация");
+
+            Assert.AreEqual("нет да да да нет нет ", message);
+        }
+
+        [TestMethod()]
+        public void answerTest2()
+        {
+            var message = Logic.answer("qweasdewqqwe", "qwe");
+
+            Assert.AreEqual("да да да нет нет нет ", message);
+        }
+    }
+}
